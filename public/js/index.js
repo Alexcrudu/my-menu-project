f132e06ca24bada5ea71a3fc55f55d1b0f12bc8c
Alexcrudu <60149530+Alexcrudu@users.noreply.github.com>
2021-10-21 08:50:25 +0200

    user login server

--- public/js/index.js
+++ public/js/index.js
@@ -1,55 +1,75 @@
-'use strict'
+"use strict";
+
+// const { response } = require("express");
 
 // CONSTANTS
-const header = document.querySelector('.header')
+const header = document.querySelector(".header");
 const navToggler = document.querySelector(".nav-toggler");
-const menuTabs = document.querySelector('.menu-tabs')
+const menuTabs = document.querySelector(".menu-tabs");
+const inputUser = document.querySelector("#inputUser");
+const inputPass = document.querySelector("#inputPass");
+const buttonLogin = document.querySelector("#buttonLogin");
+
+const login = () => {
+  const loginRequest = new Request("/login", {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      user: inputUser.value,
+      pass: inputPass.value,
+    }),
+  });
+  console.log();
+
+  fetch(loginRequest)
+    .then((response) => response.text())
+    .then((response) => console.log(response))
+    .catch(console.log);
+};
+
+buttonLogin.addEventListener("click", login);
 
 // FUNCTION
 
 const toggleNav = () => {
-    navToggler.classList.toggle('active');
-    document.querySelector('.nav').classList.toggle('open')
-
-}
-
-// init
-
-const init = () => {
-
-}
-
+  navToggler.classList.toggle("active");
+  document.querySelector(".nav").classList.toggle("open");
+};
 
 // events
 
-navToggler.addEventListener('click', toggleNav);
+navToggler.addEventListener("click", toggleNav);
 
-document.addEventListener('click', (evt)=> {
-    if(evt.target.closest('.nav-item')){
-        toggleNav();
-    }
-})
+document.addEventListener("click", (evt) => {
+  if (evt.target.closest(".nav-item")) {
+    toggleNav();
+  }
+});
 
-window.addEventListener('scroll', ()=> {
-    // console.log(pageYOffset);
-    if(pageYOffset > 60){
-        header.classList.add('sticky')
-    }else {
-        header.classList.remove('sticky')
-    }
-})
+window.addEventListener("scroll", () => {
+  // console.log(pageYOffset);
+  if (pageYOffset > 60) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
 
-menuTabs.addEventListener('click', (evt)=> {
-    if (evt.target.classList.contains('menu-tab-item') && !evt.target.classList.contains('active')){
-        const target = evt.target.getAttribute('data-target');
-        menuTabs.querySelector('.active').classList.remove('active');
-        evt.target.classList.add('active');
-        const menuSection = document.querySelector('.menu-section');
-        menuSection.querySelector('.menu-tab-content.active').classList.remove('active');
-        menuSection.querySelector(`#${target}`).classList.add('active')
-    }
-})
-
-// init()
-
-init()
+menuTabs.addEventListener("click", (evt) => {
+  // debugger
+  if (
+    evt.target.classList.contains("menu-tab-item") &&
+    !evt.target.classList.contains("active")
+  ) {
+    const target = evt.target.getAttribute("data-target");
+    menuTabs.querySelector(".active").classList.remove("active");
+    evt.target.classList.add("active");
+    const menuSection = document.querySelector(".menu-section");
+    menuSection
+      .querySelector(".menu-tab-content.active")
+      .classList.remove("active");
+    menuSection.querySelector(`#${target}`).classList.add("active");
+  }
+});
--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const e = require('express');
 const express = require('express')
+const fs = require('fs');
+
+const pfade = {
+    file: 'data/user.json'
+}
 
 
 const server = express();
@@ -6,11 +12,39 @@ server.use(express.static('public', {
     extensions: ['html']
 }))
 
-server.get('/', (req, res) => {
-    res.sendFile(path.join('/public/index.html'))
+server.use(express.json())
+
+server.post('/login',(req, res)=> {
+    let user = req.body.user;
+    let pass = req.body.pass;
+    console.log(user, pass);
+    fs.readFile(pfade.file, (err, content)=>{
+        if(err) return;
+        else {content = JSON.parse(content);
+            console.log(content);
+
+            if(user != content[0].user) {
+                res.send('no')
+
+            } else {
+                console.log('yes user, pass?');
+                if(pass != content[0].pass){
+                    res.send('no')
+                    console.log('no pass');
+                } else {
+                    console.log('yes user and pass');
+                    res.send('super')
+                }
+            }
+        }
+    });
 })
+
+// server.get('/', (req, res) => {
+//     res.send('public/index.html')
+// })
 
 server.listen(80, err => {
     if(err) console.log(err);
-    else console.log('server auft');
+    else console.log('OK');
 })
